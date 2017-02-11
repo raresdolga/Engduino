bcaa56aa102f3bb665344811f66d29646d1b7fa5
Jacob Moss <cob.mossy@gmail.com>
2017-02-11 22:00:21 +0000

    add light sensor

--- code_sensors/code_sensors.ino
+++ code_sensors/code_sensors.ino
@@ -1,30 +1,33 @@
 #include <EngduinoLEDs.h>
 #include <EngduinoButton.h>
-#include <EngduinoThermistor.h> 
+#include <EngduinoThermistor.h>
 #include <EngduinoLight.h>
 #include <EngduinoAccelerometer.h>
 #include <EngduinoMagnetometer.h>
-#include <Wire.h> 
 #include <Wire.h>
-#include<stdlib.h>
+#include <Wire.h>
+#include <stdlib.h>
 int state=0;
 String h;
+long lastCall;
+
 void setup() {
   // put your setup code here, to run once:
-EngduinoLEDs.begin(); 
-EngduinoButton.begin(); 
-EngduinoThermistor.begin();
-EngduinoLight.begin();
-EngduinoAccelerometer.begin(); 
-EngduinoMagnetometer.begin();
-firstContact();
+  EngduinoLEDs.begin();
+  EngduinoButton.begin();
+  EngduinoThermistor.begin();
+  EngduinoLight.begin();
+  EngduinoAccelerometer.begin();
+  EngduinoMagnetometer.begin();
+  firstContact();
+  lastCall = millis();
 }
 
 void getInput()
 {
   int from=0,to=0,i=0;
- 
-  if (Serial.available() > 0) 
+
+  if (Serial.available() > 0)
   {
     int *values=(int*)malloc(sizeof(int)*6);
     // If data is available to read,
@@ -33,21 +36,22 @@ void getInput()
     {
      from=to;
       while(h.charAt(to)!=','&&to<h.length())
-      {  
+      {
          to++;
       }
       *(values+i)=(h.substring(from,to)).toInt(); // take the part of string which is between comas and convert it into int
-      i+=1; 
+      i+=1;
     to++;//skip the coma
   }
   execute(values);
-  free(values);//clean memory 
+  free(values);//clean memory
   }
-  else
+  else if (millis() - lastCall > 200)
   {
+    lastCall = millis();
     printFunction();
   }
- 
+
 }
 //for more commands
 void execute(int *values)
@@ -57,7 +61,7 @@ void execute(int *values)
     case 1: lights(values);
     break;
     default: ;
-  } 
+  }
 }
 
 void lights(int *values)
@@ -65,7 +69,7 @@ void lights(int *values)
   if(*(values+2)==0)
     {
   EngduinoLEDs.setLED(*(values+1),OFF);
-  return;  //break 
+  return;  //break
     }
     EngduinoLEDs.setLED(*(values+1),*(values+3),*(values+4),*(values+5));
 }
@@ -78,7 +82,7 @@ float x = accelerations[0];
 float y = accelerations[1];
 float z = accelerations[2];
 Serial.print("1: ");
-float a = sqrt(x*x + y*y + z*z); 
+float a = sqrt(x*x + y*y + z*z);
 Serial.println(a);
 
 if(EngduinoButton.wasPressed()) state=!state;
@@ -87,10 +91,10 @@ Serial.println(state);
 
 Serial.print("3: ");
 float f;
-f = EngduinoThermistor.temperature(CELSIUS); 
+f = EngduinoThermistor.temperature(CELSIUS);
 Serial.println(f);
 float magneticField[3];
-EngduinoMagnetometer.xyz(magneticField); 
+EngduinoMagnetometer.xyz(magneticField);
 float d = magneticField[0];
 float b = magneticField[1];
 float c = magneticField[2];
@@ -99,19 +103,17 @@ Serial.print(d);
 Serial.print(" ");
 Serial.print(b);
 Serial.print(" ");
-Serial.println(c); 
+Serial.println(c);
+Serial.print("5: ");
+Serial.println((int) 100 * (EngduinoLight.lightLevel() / 1023.0));
 }
 void firstContact() {
   while (Serial.available() <= 0) {
-  printFunction();  
+  printFunction();
   delay(100);
   }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-getInput();
-delay(100); 
-
-
+   getInput();
 }
